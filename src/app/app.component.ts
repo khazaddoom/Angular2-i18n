8db0161536beee8b7786063134609d333ec1b322
Ganesh Acharya <ganesh.a@juegostudio.com>
2019-07-11 12:28:52 +0530

    initial files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,42 @@ import {TranslateService} from '@ngx-translate/core';
   selector: 'app-root',
   template: `
     <div>
-      <h2>{{ 'HOME.TITLE' | translate }}</h2>
+      <h2>{{ 'TITLE' | translate }}</h2>
       <label>
-        {{ 'HOME.SELECT' | translate }}
-        <select #langSelect (change)="translate.use(langSelect.value)">
+        {{ 'SELECT' | translate }}
+        <select #langSelect (change)="onToggleLanguage(langSelect.value)">
           <option *ngFor="let lang of translate.getLangs()" [value]="lang" [selected]="lang === translate.currentLang">{{ lang }}</option>
         </select>
       </label>
+      <a (click) = "selectMenu(myHome)"> {{ 'home' | translate}}</a>
     </div>
   `,
 })
 export class AppComponent {
+
+  direction = '';
+  myHome = 'home';
+
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'fr']);
+    translate.addLangs(['en', 'fr', 'ar']);
     translate.setDefaultLang('en');
 
     const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    translate.use(browserLang.match(/en|ar/) ? browserLang : 'en');
   }
+
+  onToggleLanguage(lang) {
+
+    this.direction = lang === 'ar' ? "rtl" : 'ltr';
+
+    document.body.dir = this.direction;
+
+    this.translate.use(lang);
+  }
+
+  selectMenu(myHome) {
+    console.log(myHome)
+  }
+
+
 }
